bb71069a6a83d9ead85b39e520ebd1fbe5779cf1
ftolentino <filmer.tolentino@gmail.com>
2022-06-02 16:07:19 -0700

    ADD branching for soulmate match
    Co-authored-by: Winnie Wang <weijwang18@gmail.com>
    Co-authored-by: Matt Herbert <matthew.f.herbert@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,54 @@
 $(document).ready(function() {
-  $("form#insurance").submit(function(event) {
-    const age = parseInt($("input#age").val());
-    const gender = $("select#gender").val();
-
-    if (age) {
-      let quote = (100 - age) * 3;
-      if (gender === 'male' && age < 26) {
-        quote += 50;
-      }
-
-      $("#rate").text(quote);
-      $("#quote").show();
-    } else {
-      alert('Please enter your age.');
-    }
-
+  $("form#foodForm").submit(function(event) {
     event.preventDefault();
+    const foodInput = $("input:radio[name=Food]:checked").val();
+    const animalInput = $("input:radio[name=Animal]:checked").val();
+
+    if (foodInput === 'Candy' && animalInput === 'Cat') {
+      $('.output').text('Johnny Depp');
+    } else if (foodInput === 'Candy' && animalInput === 'Dog'){
+      $('.output').text('Robin Williams');
+    } else if (foodInput === 'Steak' && animalInput === 'Cat') {
+      $('.output').text('Anne Hathaway'); 
+    } else {
+      $('.output').text('Amber Herd');
+    }
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//   $("form#insurance").submit(function(event) {
+//     const age = parseInt($("input#age").val());
+//     const gender = $("select#gender").val();
+
+//     if (age) {
+//       let quote = (100 - age) * 3;
+//       if (gender === 'male' && age < 26) {
+//         quote += 50;
+//       }
+
+//       $("#rate").text(quote);
+//       $("#quote").show();
+//     } else {
+//       alert('Please enter your age.');
+//     }
+
+//     event.preventDefault();
+//   });
+// });
